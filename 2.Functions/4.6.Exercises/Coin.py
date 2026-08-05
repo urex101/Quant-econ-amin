124904fca1d6642700fc237c8a1d2596aa96da90
Amin <aminchnejad@gmail.com>
2026-08-05 16:40:54 +0000

    Aug 5, 2026, 7:40 PM

---
+++ 2.Functions/4.6.Exercises/Coin.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+rng = np.random.default_rng()
+k = 3
+heads = 0
+tails = 0
+money = 0
+coin_list = []
+for _ in range(10):
+    coin = rng.choice([-1, 1], p=[0.5, 0.5])
+    coin_list.append(coin)
+    if coin == 1:
+        heads += 1
+    if coin == -1:
+        tails += 1
+
+for coin in coin_list:
+    if coin == 1 and coin_list[coin:coin+k] == [1]*k:
+        money += 1
+    if coin == -1 and coin_list[coin:coin+k] == [-1]*k:
+        money += 1
+
+print("money:", money)
