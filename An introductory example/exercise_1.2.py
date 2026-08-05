1db633d4bf3e4f9a4352fb5941beeb6bb01381d2
Amin <aminchnejad@gmail.com>
2026-08-05 15:07:55 +0000

    Aug 5, 2026, 6:07 PM

--- An introductory example/exercise_1.2.py
+++ An introductory example/exercise_1.2.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 T = 200 
 a = [0,0.8,0.98]
 x = np.empty(T)
-x[0] = 0
 
 for a_i in a:
-
-        for t in range(T - 1):
+    x[0] = 0
+    for t in range(T - 1):
             x[t + 1] = a_i * x[t] + np.random.standard_normal()
-
-        plt.plot(x, label=f'a = {a_i}')
+            plt.plot(x, label=f'a = {a_i}')
 
 plt.legend()
 plt.show()
