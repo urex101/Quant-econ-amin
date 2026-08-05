528e188660a07a8d7ab9af07d3682331d75de938
Amin <aminchnejad@gmail.com>
2026-08-05 16:00:13 +0000

    Aug 5, 2026, 7:00 PM

--- 2.Functions/4.6.Exercises/binomial_vari.py
+++ 2.Functions/4.6.Exercises/binomial_vari.py
@@ -3,4 +3,13 @@ import numpy as np
 rng = np.random.default_rng()
 
 def binomial_rv(n,p):
+        w = int(0)
         for i in range(n):
+            land = rng.standard_normal()
+            if float(land) < float(p):
+                w += 1
+            else:
+                pass
+        return w
+
+print (binomial_rv(10,0.5))
