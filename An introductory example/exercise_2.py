eeba20bbbcf4f5c9f39194e378debc5e40be5257
Amin <aminchnejad@gmail.com>
2026-08-05 15:11:06 +0000

    Aug 5, 2026, 6:11 PM

---
+++ An introductory example/exercise_2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#simulate and plot the correlated time series
+
+T = 200 
+a = 0.9
+x = np.empty(T)
+x[0] = 0
+for t in range(T - 1):
+    x[t + 1] = a * np.abs(x[t]) + np.random.standard_normal()
+
+
+plt.plot(x)
+plt.show()
