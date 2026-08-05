6170dc5109155b73850f2007120c776976f5d0f4
Amin <aminchnejad@gmail.com>
2026-08-05 16:03:34 +0000

    Aug 5, 2026, 7:03 PM

--- 2.Functions/4.6.Exercises/binomial_vari.py
+++ 2.Functions/4.6.Exercises/binomial_vari.py
@@ -5,7 +5,7 @@ rng = np.random.default_rng()
 def binomial_rv(n,p):
         w = int(0)
         for i in range(n):
-            land = rng.standard_normal()
+            land = rng.uniform(0,1)
             if float(land) < float(p):
                 w += 1
             else:
