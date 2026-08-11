d2a6ea677f06e253fcbb964e02075e94b7533678
Amin <aminchnejad@gmail.com>
2026-08-11 17:21:23 +0000

    Aug 11, 2026, 8:21 PM

--- 3.Python Essentials/5.7. Exercises/Exercise 5.1.py
+++ 3.Python Essentials/5.7. Exercises/Exercise 5.1.py
@@ -10,3 +10,12 @@ x_vals = [1, 2, 3]
 y_vals = [4, 5, 6]
 sum_product = sum(x * y for x, y in zip(x_vals, y_vals))
 print(sum_product)
+
+
+
+sum_even = sum(1 for x in range(100) if x % 2 == 0)
+print(sum_even)
+
+pairs = ((2, 5), (4, 2), (9, 8), (12, 10))
+even_pairs_count = sum(1 for a, b in pairs if a % 2 == 0 and b % 2 == 0)
+print(even_pairs_count)
