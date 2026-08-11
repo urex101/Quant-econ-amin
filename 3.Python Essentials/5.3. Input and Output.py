761b61ec3f0c8bd03602f49ffcc222c9ff685382
Amin <aminchnejad@gmail.com>
2026-08-11 16:49:17 +0000

    Aug 11, 2026, 7:49 PM

--- 3.Python Essentials/5.3. Input and Output.py
+++ 3.Python Essentials/5.3. Input and Output.py
@@ -1,3 +1,17 @@
 with open('newfile.txt', 'w') as f:  
     f.write('Testing\n')         
     f.write('Testing again')
+
+
+
+with open('newfile.txt', 'r') as fo:
+    out = fo.read()
+    print(out)
+
+
+
+with open("newfile.txt", "r") as f:
+    file = f.readlines()
+    with open("output.txt", "w") as fo:
+        for i, line in enumerate(file):
+            fo.write(f'Line {i}: {line} \n')
