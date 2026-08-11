7f5ed0b2f27bf27df938441cd7ce75f6f8f62366
Amin <aminchnejad@gmail.com>
2026-08-11 17:16:32 +0000

    Aug 11, 2026, 8:16 PM

--- 3.Python Essentials/5.7. Exercises/Exercise 5.1.py
+++ 3.Python Essentials/5.7. Exercises/Exercise 5.1.py
@@ -1,1 +1,11 @@
+"""
+Part 1: Given two numeric lists or tuples x_vals and y_vals of equal length, compute their inner product using zip().
 
+Part 2: In one line, count the number of even numbers in 0,…,99.
+
+Part 3: Given pairs = ((2, 5), (4, 2), (9, 8), (12, 10)), count the number of pairs (a, b) such that both a and b are even.
+"""
+
+x_vals = [1, 2, 3]
+y_vals = [4, 5, 6]
+zipped = zip(x_vals, y_vals)
